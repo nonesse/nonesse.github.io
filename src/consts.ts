76e0140a64aa22443ff1657bde56e2b381360c54
nonesse <>
2025-03-02 22:58:11 +0100

    Adapt site

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Black Fortress",
+  DESCRIPTION: "Welcome to Black Fortress, my hidden corner on the Internet.",
+  AUTHOR: "nonesse",
 }
 
 // Work Page
@@ -38,22 +38,22 @@ export const LINKS: Links = [
     HREF: "/", 
   },
   { 
-    TEXT: "Work", 
-    HREF: "/work", 
-  },
-  { 
     TEXT: "Blog", 
     HREF: "/blog", 
   },
-  { 
+  /*{ 
     TEXT: "Projects", 
     HREF: "/projects", 
   },
+  { 
+    TEXT: "Work", 
+    HREF: "/work", 
+  },*/
 ]
 
 // Socials
 export const SOCIALS: Socials = [
-  { 
+  /*{ 
     NAME: "Email",
     ICON: "email", 
     TEXT: "markhorn.dev@gmail.com",
@@ -76,6 +76,11 @@ export const SOCIALS: Socials = [
     ICON: "twitter-x",
     TEXT: "markhorn_dev",
     HREF: "https://twitter.com/markhorn_dev",
-  },
+  },*/
 ]
 
+export const LANG_ES = "es"
+export const LANG_EN = "en"
+export const NO_SHERLOCK_HINT_ES = "Esta pregunta no tiene pista."
+export const NO_SHERLOCK_HINT_EN = "No hint for this question."
+export const URL_SHERLOCK_SOLVED_GIF = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWh5ZDZ4dmc2cXRiaGN1cndtazV5eHNzMjE5NGcxZ3VxYTB6MThvNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/P3gCL7t3cbOWUN8ma7/giphy.gif"
